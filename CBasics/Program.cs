49acb74dcb7c644903fdbf62afa2eaf79c4651d6
Jacob Hock <98995977+23jhock@users.noreply.github.com>
2024-08-28 11:42:52 -0500

    Add files via upload

---
+++ CBasics/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CBasics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        //Declare a constant of type byte named sample1 with an initial value of 0x3A
+        public const byte sample1 = 0x3A;
+
+        //Declare a variable of type byte named sample2 with an initial value of 58
+
+        //Declare a variable of type int named heartRate with an initial value of 85
+
+        //Declare a variable of type double named deposits that has an initial value of 135002796
+
+        //Declare a constant float named acceleration that has an initial value of 9.800
+
+        //Declare a variable float named mass that has an initial value of 14.6
+
+        //Declare a variable double named distance that has an initial value of 129.763001
+
+        //Declare a variable bool named lost that has an initial value of true
+
+        //Declare a variable bool named expensive that has an initial value of true
+
+        //Declare a variable int named choice with an initial value of 2
+
+        //Declare a constant of type char named integral that has a value of \u222B
+
+        //Create a constant String named greeting that has an initial value of “Hello”
+
+        //Create a variable String named name that has an initial value of “Karen”
+
+    }
+}
